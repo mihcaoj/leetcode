4ac4f1f3f19d4d6852d1dc483efb4a7e111fe310
Joachim Bono <joachim.bono@protonmail.com>
2025-06-08 11:12:01 +0200

    Add problem 101

---
+++ 101_symmetric_tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Time: O(n) / Space: O(h)
+    def is_mirror(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        if left and right:
+            return (left.val == right.val and
+                self.is_mirror(left.left, right.right) and
+                self.is_mirror(left.right, right.left))
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        return self.is_mirror(root.left, root.right)
