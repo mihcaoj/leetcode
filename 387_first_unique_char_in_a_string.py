888c797ebe7d4040cb77fada073477e7f6f263b0
Joachim Bono <joachim.bono@protonmail.com>
2025-06-19 11:43:56 +0200

    Add problem 387

---
+++ 387_first_unique_char_in_a_string.py
@@ -0,0 +1,24 @@
+class Solution:
+    # Dict solution (Time: O(n) / Space: O(n))
+    def firstUniqChar(self, s: str) -> int:
+        char_count = {}
+
+        # Count
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Find index of first char in string
+        for i, char in enumerate(s):
+            if char_count[char] == 1:
+                return i
+        # No unique char found
+        return -1
+
+    # Counter solution (Time: O(n) / Space: O(n))
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+
+        for i, char in enumerate(s):
+            if count[char] == 1:
+                return i
+        return -1
