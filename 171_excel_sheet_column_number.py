14eaf5d08ac106195552bb0ced2730baed602fd9
Joachim Bono <joachim.bono@protonmail.com>
2025-06-26 18:36:58 +0200

    Add problem 232

--- 171_excel_sheet_column_number.py
+++ 171_excel_sheet_column_number.py
@@ -1,8 +1,10 @@
 class Solution:
+    # Time: O(n) / Space: O(1)
     def titleToNumber(self, columnTitle: str) -> int:
         res = 0
 
         for char in columnTitle:
+            # integer code point, normalize to 0, add 1 to make it 1-based instead of 0-based
             val = ord(char) - ord('A') + 1
             res = res * 26 + val
         return res
---
+++ 232_implement_queue_using_stacks.py
@@ -0,0 +1,39 @@
+class MyQueue:
+    # With a single deque (Time: O(1) / Space: O(n))
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not self.queue
+
+    # Solution that uses two deques (Time: O(1) / Space: O(n))
+    def __init__(self):
+        self.stackin = deque()
+        self.stackout = deque()
+
+    def push(self, x: int) -> None:
+        self.stackin.append(x)
+
+    def pop(self) -> int:
+        if not self.stackout:
+            while self.stackin:
+                self.stackout.append(self.stackin.popleft())
+        return self.stackout.popleft()
+
+    def peek(self) -> int:
+        if not self.stackout:
+            while self.stackin:
+                self.stackout.append(self.stackin.popleft())
+        return self.stackout[0]
+
+    def empty(self) -> bool:
+        return not self.stackin and not self.stackout
