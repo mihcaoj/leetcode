9d5bbdf2f4764921acf00d890db32b4c8dd5979f
Joachim Bono <joachim.bono@protonmail.com>
2025-06-26 19:37:35 +0200

    Add problem 1

---
+++ 1.two_sum.py
@@ -0,0 +1,25 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+class Solution:
+    # Naive solution (Time: O(n^2) / Space: O(n^2))
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        j = 1
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    # Optimal solution (Time: O(n) / Space: O(n))
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
