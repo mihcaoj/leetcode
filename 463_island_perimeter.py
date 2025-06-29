048823dd0ef5e90abf156232224887b2f70071c4
Joachim Bono <joachim.bono@protonmail.com>
2025-06-29 15:29:33 +0200

    Add problem 695

--- 463_island_perimeter.py
+++ 463_island_perimeter.py
@@ -1,18 +1,20 @@
 '''
 You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
-Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
-The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+Grid cells are connected horizontally/vertically (not diagonally).
+The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island.
+One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100.
+Determine the perimeter of the island.
 '''
 class Solution:
     # Time: O(n*m) / Space: O(1)
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         perimeter = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
+        rows, cols = len(grid), len(grid[0])
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]:
                     perimeter += 4
-                    if i > 0 and grid[i-1][j] == 1:
-                        perimeter -= 2
-                    if j > 0 and grid[i][j-1] == 1:
-                        perimeter -= 2
+                    if i > 0 and grid[i - 1][j] == 1: perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1: perimeter -= 2
         return perimeter
---
+++ 695_max_area_of_island.py
@@ -0,0 +1,33 @@
+'''
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid.
+If there is no island, return 0.
+'''
+class Solution:
+    # Time: O(n*m) / Space: O(n*m)
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]: return 0
+
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if not (0 <= r < rows and 0 <= c < cols): return 0
+            if grid[r][c] == 0: return 0
+
+            grid[r][c] = 0
+            area = 1
+            area += dfs(r + 1, c)
+            area += dfs(r - 1, c)
+            area += dfs(r, c - 1)
+            area += dfs(r, c + 1)
+            return area
+
+        max_area = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]:
+                    curr_area = dfs(i, j)
+                    max_area = max(curr_area, max_area)
+        return max_area
