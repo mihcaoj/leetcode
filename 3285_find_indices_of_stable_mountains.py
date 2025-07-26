a3dd4aae8a5d4567cc86af6033370f288168ec31
Joachim Bono <joachim.bono@protonmail.com>
2025-07-26 21:14:38 +0200

    Add problem 3285

---
+++ 3285_find_indices_of_stable_mountains.py
@@ -0,0 +1,18 @@
+'''
+There are n mountains in a row, and each mountain has a height. You are given an integer array height where height[i] represents the height of mountain i, and an integer threshold.
+A mountain is called stable if the mountain just before it (if it exists) has a height strictly greater than threshold.
+Note that mountain 0 is not stable.
+Return an array containing the indices of all stable mountains in any order.
+'''
+class Solution:
+    # Time: O(n) / Space: O(n)
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        stable_mountains = []
+        for i in range(1, len(height)):
+            if height[i - 1] > threshold:
+                stable_mountains.append(i)
+        return stable_mountains
+
+        # List comprehension one liner (Time: O(n) / Space: O(n))
+        def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+            return [i for i in range(1, len(height)) if height[i - 1] > threshold]
