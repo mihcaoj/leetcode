b5482206d75eac6ad0d0b971ca73669c28843270
Joachim Bono <joachim.bono@protonmail.com>
2025-08-25 15:49:30 +0200

    Add problem 1

---
+++ typescript/1_two_sum.ts
@@ -0,0 +1,27 @@
+// Naive solution (Time: O(n) / Space: O(n))
+function twoSumNaive(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+// Optimal solution (Time: O(n) / Space: O(n))
+function twoSumOptimal(nums: number[], target: number): number[] {
+  const numMap = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
+
+    if (numMap.has(complement)) {
+      return [numMap.get(complement)!, i];
+    }
+    numMap.set(num, i);
+  }
+  return [];
+}
