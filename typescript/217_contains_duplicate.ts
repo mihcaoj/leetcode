1ff9e030e0ad782686070d376ed74f9bb190eda1
Joachim Bono <joachim.bono@protonmail.com>
2025-08-25 16:51:37 +0200

    Add problem 217

---
+++ typescript/217_contains_duplicate.ts
@@ -0,0 +1,12 @@
+// Time: O(n) / Space: O(n)
+function containsDuplicate(nums: number[]): boolean {
+  const mySet = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (mySet.has(nums[i])) {
+      return true;
+    }
+    mySet.add(nums[i]);
+  }
+  return false;
+}
