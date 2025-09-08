c4fc71e44964fb2eecf604871f48693bd3502524
Joachim Bono <joachim.bono@protonmail.com>
2025-09-08 16:03:20 +0200

    Add problem 18

---
+++ typescript/18_four_sum.ts
@@ -0,0 +1,35 @@
+// Time: O(n^3) / Space: O(1)
+function fourSum(nums: number[], target: number): number[][] {
+  nums.sort((a, b) => a - b); // sort ascending
+  let result: number[][] = [];
+  let n = nums.length;
+
+  for (let i = 0; i < n - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    for (let j = i + 1; j < n - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      let left = j + 1;
+      let right = n - 1;
+      while (left < right) {
+        let total = nums[i] + nums[j] + nums[left] + nums[right];
+
+        if (total < target) {
+          left++;
+        } else if (total > target) {
+          right--;
+        } else {
+          result.push([nums[i], nums[j], nums[left], nums[right]]);
+
+          while (left < right && nums[left] === nums[left + 1]) left++;
+          while (left < right && nums[right] === nums[right - 1]) right--;
+
+          left++;
+          right--;
+        }
+      }
+    }
+  }
+
+  return result;
+}
