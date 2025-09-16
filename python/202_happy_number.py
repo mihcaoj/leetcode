b0accc54441bbd2a29985398af26a86cb22e5f07
Joachim Bono <joachim.bono@protonmail.com>
2025-09-16 11:32:36 +0200

    Add problem 202

---
+++ python/202_happy_number.py
@@ -0,0 +1,14 @@
+# Time: O(logn) / Space: O(logn)
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1:
+            if n in seen:
+                return False  # cycle
+            seen.add(n)
+            digits = [int(d) for d in str(n)]
+            sum_of_squares = sum(digit**2 for digit in digits)
+            n = sum_of_squares
+
+        return True
